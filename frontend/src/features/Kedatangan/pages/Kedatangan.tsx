26fd7f222af034f5af041da1e416f03e82fc1e0a
wj <151712052+ThangWeiJie@users.noreply.github.com>
2025-12-11 20:57:50 +0800

    Updated attendance

--- frontend/src/features/Kedatangan/pages/Kedatangan.tsx
+++ frontend/src/features/Kedatangan/pages/Kedatangan.tsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import { checkIP } from "../api/fetchApi";
 import AttendancePage from "./ClockInPage";
+import UnauthorizedPage from "./Unauthorised";
+import LoadingSpinner from "../../../components/LoadingSpinner";
 
 export default function Kedatangan() {
 
   const [allowed, setAllowed] = useState<boolean | null>(true);
 
-  // useEffect(() => {
-  //   checkIP().then(setAllowed);
-  // }, []);
+  useEffect(() => {
+    checkIP().then(setAllowed);
+  }, []);
 
-  if(allowed === null) return <div>Checking network...</div>;
-  if(!allowed) return <div>Not Authorised</div>
+  if(allowed === null) return <div><LoadingSpinner/></div>;
+  if(!allowed) {
+    return <UnauthorizedPage />
+  }
 
   return(
     <AttendancePage/>
